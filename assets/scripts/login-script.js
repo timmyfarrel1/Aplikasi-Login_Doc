888a03bfafc97ad4bd0a99ec1a8da707316ab77c
timmyfarrel1 <165824898+timmyfarrel1@users.noreply.github.com>
2024-09-14 10:26:08 +0700

    Update login-script.js
    Melakukan revisi format komentar sesuai dengan JSDoc

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -4,18 +4,44 @@
  * Gunakan fungsi goToLogin() untuk menampilkan halaman login
  * Gunakan fungsi showPopUp() untuk menampilkan pop up error
  */
-
+/**@module Login-Script */
+/** 
+*Membuat variable loginFormElement untuk tampilan form.
+*@constant{HTMLElement}
+*/
 const loginFormElement = document.querySelector('#loginForm');
+/**
+*Membuat variable inputEmailElement untuk tampilan input email
+*constant{HTMLElement}
+*/
 const inputEmailElement = document.querySelector('#inputEmail');
+/**
+*Membuat variable inputPasswordElement untuk tampilan input hasil.
+*constant{HTMLElement}
+*/
 const inputPasswordElement = document.querySelector('#inputPassword');
-
+/**
+*Membuat variable expectedEmail untuk menyimpan informasi email sementara.
+@constant {string}
+*/
 const expectedEmail = 'admin@dicoding.com';
+/**
+*Membuat variable expectedPassword untuk menyimpan informasi password sementara.
+@constant {string}
+*/
 const expectedPassword = 'superpassword';
 
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
-
+  /**
+  *Membuat variable email untuk menyimpan nilai email yang didapatkan saat button ditekan.
+  *@constant {string}
+  */
   const email = inputEmailElement.value;
+  /**
+  *Membuat variable password untuk menyimpan nilai password yang didapatkan saat button ditekan.
+  @constant {string}
+  */
   const password = inputPasswordElement.value;
   if (email == expectedEmail && password == expectedPassword) {
     goToHome();
